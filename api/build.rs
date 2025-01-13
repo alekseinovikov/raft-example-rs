9aa3c9541404941a52e219fece0f0592a72b18fd
alekseinovikov <novikov.aleksei.92@gmail.com>
2025-01-13 21:23:38 +0200

    Added support of vendored protoc with the dependencies

--- api/build.rs
+++ api/build.rs
@@ -1,4 +1,18 @@
+use std::env;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("src/api.proto")?;
+    let protoc = protoc_bin_vendored::protoc_bin_path();
+    match protoc {
+        Ok(path) => {
+            env::set_var("PROTOC", path);
+            tonic_build::configure()
+                .compile_protos(&["src/api.proto"], &["src"])?;
+        }
+        Err(err) => {
+            eprintln!("protoc not found: {}", err);
+            return Err(Box::new(err));
+        },
+    }
+
     Ok(())
 }
